34a781cfc1d5ce2b58aca57bbda3eb9ff9d40226
전준원 <drjeon92@gmail.com>
2022-07-31 17:23:44 +0900

    로드밸런싱까지 완료

--- techefx-product-enquiry-service/src/main/java/com/techefx/microservices/productstockservice/techefxproductenquiryservice/client/ProductStockClient.java
+++ techefx-product-enquiry-service/src/main/java/com/techefx/microservices/productstockservice/techefxproductenquiryservice/client/ProductStockClient.java
@@ -1,4 +1,4 @@
-package com.techefx.microservices.productstockservice.techefxproductenquiryservice.client;
+ package com.techefx.microservices.productstockservice.techefxproductenquiryservice.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -6,7 +6,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.techefx.microservices.productstockservice.techefxproductenquiryservice.beans.ProductEnquiryBean;
 
-@FeignClient(name="techefx-product-stock-service", url="localhost:8800")
+//@FeignClient(name="techefx-product-stock-service", url="localhost:8800")
+@FeignClient(name="techefx-product-stock-service")
 public interface ProductStockClient {
 	@GetMapping("/check-product-stock/productName/{productName}/productAvailability/{productAvailability}")
 	public ProductEnquiryBean checkProdutStock(@PathVariable("productName") String productName, @PathVariable("productAvailability") String productAvailability);
