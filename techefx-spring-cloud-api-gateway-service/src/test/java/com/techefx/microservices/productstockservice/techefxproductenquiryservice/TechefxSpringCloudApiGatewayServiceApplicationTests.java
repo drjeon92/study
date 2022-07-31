5b9d7bbed4a15f436c2e630e7cf4cdca6d1ea39a
전준원 <drjeon92@gmail.com>
2022-07-31 17:29:18 +0900

    api gateway service

---
+++ techefx-spring-cloud-api-gateway-service/src/test/java/com/techefx/microservices/productstockservice/techefxproductenquiryservice/TechefxSpringCloudApiGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techefx.microservices.productstockservice.techefxproductenquiryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechefxSpringCloudApiGatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
